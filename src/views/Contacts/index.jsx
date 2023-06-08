052117e4261c5f460730ef9425c8f15f4459d830
HoHoangHuyQuang <huyquang2842@gmail.com>
2023-06-08 17:57:53 +0700

    add form

--- src/views/Contacts/index.jsx
+++ src/views/Contacts/index.jsx
@@ -1,22 +1,31 @@
-
-import { Box } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { mockDataContacts } from '../../data/mockContact';
+import { Box } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { mockDataContacts } from "../../data/mockContact";
 
 const columns = [
-    { field: 'id', headerName: 'ID', flex: 0.5 },
-    { field: 'name', headerName: 'Name', flex: 1 },
-    { field: 'email', headerName: 'Email', flex: 1 },
-    { field: 'age', headerName: 'Age', flex: 0.5 },
-    { field: 'phone', headerName: 'Phone', flex: 1 },
-    { field: 'address', headerName: 'Address', flex: 3 },
-    { field: 'city', headerName: 'City', flex: 1.5 },
-    { field: 'zipCode', headerName: 'Zip Code', flex: 1 },
-]
+  { field: "id", headerName: "ID", flex: 0.5 },
+  { field: "name", headerName: "Name", flex: 1 },
+  { field: "email", headerName: "Email", flex: 1 },
+  { field: "age", headerName: "Age", flex: 0.5 },
+  { field: "phone", headerName: "Phone", flex: 1 },
+  { field: "address", headerName: "Address", flex: 3 },
+  { field: "city", headerName: "City", flex: 1.5 },
+  { field: "zipCode", headerName: "Zip Code", flex: 1 },
+];
 
 const ContactsPage = () => {
-    return(
-        <Box sx={{ height: '75vh', width: '80vw' }}>
+  return (
+    <div className="page-content">
+      <div>
+        <h1>Contact pages</h1>
+      </div>
+
+      <Box
+        sx={{
+          flex: "auto",
+          width: "100vw",
+        }}
+      >
         <DataGrid
           rows={mockDataContacts}
           columns={columns}
@@ -32,6 +41,7 @@ const ContactsPage = () => {
           disableRowSelectionOnClick
         />
       </Box>
-    );
-}
-export default ContactsPage
+    </div>
+  );
+};
+export default ContactsPage;
